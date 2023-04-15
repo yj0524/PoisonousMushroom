bb332d539a618669327a07e787035ebb35bb6c59
yj0524 <jeonyunjae_kr@naver.com>
2023-04-15 17:19:35 +0900

    evolve effect add

--- src/main/java/com/github/yj0524/commands/Evolve.java
+++ src/main/java/com/github/yj0524/commands/Evolve.java
@@ -3,11 +3,14 @@ package com.github.yj0524.commands;
 import com.github.yj0524.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Evolve implements CommandExecutor {
 
@@ -23,6 +26,10 @@ public class Evolve implements CommandExecutor {
             main.mushroomTeam.removeEntry(sender.getName());
             main.superMushroomTeam.addEntry(sender.getName());
             Bukkit.getPlayer(sender.getName()).getWorld().spawnEntity(Bukkit.getPlayer(sender.getName()).getLocation(), EntityType.LIGHTNING);
+            for (OfflinePlayer player : main.superMushroomTeam.getPlayers()) {
+                player.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 20, 1, false, false));
+                player.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 200, 1, false, false));
+            }
             for (Player allplayers : Bukkit.getOnlinePlayers()) {
                 allplayers.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + sender.getName() + "이(가) 슈퍼 버섯으로 진화했습니다!");
             }
